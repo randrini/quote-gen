0c90337d0d2c987eca2ce2ccc293fd92d43e81b9
Richard ANDRINIRINA <richard.andrinirina@gmail.com>
2018-04-03 23:37:48 +0200

    resp layout, initial tweet button

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const generator = document.querySelector('.box__quote-gen');
 
+const tweet = document.querySelector('.box__tweet');
+tweet.setAttribute(
+	'href',
+	'https://twitter.com/intent/tweet?text=Hello+world+%23quote'
+);
+
 generator.addEventListener('click', () => {
 	const quote = document.getElementById('quote');
+	console.log(tweet);
 	const quoteAuthor = document.querySelector('.box__cite');
 	fetch('https://talaikis.com/api/quotes/random/')
 		.then(blob => blob.json())
 		.then(data => {
+			quote.classList.add('fade');
 			quote.innerHTML = `<span>"</span>${data.quote}`;
 			quoteAuthor.innerHTML = `- ${data.author}`;
 		})
 		.catch(err => console.log(err));
 });
+
+// fitText(document.getElementById('quote'));
