625e968fe43e6516b5c5f5b7c1453b3fc592fa13
Richard ANDRINIRINA <richard.andrinirina@gmail.com>
2018-04-04 00:00:44 +0200

    Tweet button fix

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const generator = document.querySelector('.box__quote-gen');
 
-const tweet = document.querySelector('.box__tweet');
-tweet.setAttribute(
-	'href',
-	'https://twitter.com/intent/tweet?text=Hello+world+%23quote'
-);
-
 generator.addEventListener('click', () => {
 	const quote = document.getElementById('quote');
-	console.log(tweet);
 	const quoteAuthor = document.querySelector('.box__cite');
+	const tweet = document.querySelector('.box__tweet');
 	fetch('https://talaikis.com/api/quotes/random/')
 		.then(blob => blob.json())
 		.then(data => {
 			quote.classList.add('fade');
 			quote.innerHTML = `<span>"</span>${data.quote}`;
 			quoteAuthor.innerHTML = `- ${data.author}`;
+			const tweetLink = `https://twitter.com/intent/tweet?text=${
+				data.quote
+			}+%23quote`;
+			tweet.setAttribute('href', tweetLink);
 		})
 		.catch(err => console.log(err));
 });
-
-// fitText(document.getElementById('quote'));
